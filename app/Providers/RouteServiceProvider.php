6571570227d66f97ce2ae6e4e1ccf3ce8c930552
narendrafajar <100707138+narendrafajar@users.noreply.github.com>
2025-01-19 05:00:14 +0700

    Update: API prefix Route

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -28,13 +28,20 @@ class RouteServiceProvider extends ServiceProvider
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
 
-        $this->routes(function () {
-            Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('routes/api.php'));
+        // $this->routes(function () {
+        //     Route::middleware('api')
+        //         ->prefix('api')
+        //         ->group(base_path('routes/api.php'));
 
-            Route::middleware('web')
-                ->group(base_path('routes/web.php'));
+        //     Route::middleware('web')
+        //         ->group(base_path('routes/web.php'));
+        // });
+
+        $this->routes(function () {
+            Route::prefix('/')
+                ->middleware('web')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/api.php'));
         });
     }
 }
