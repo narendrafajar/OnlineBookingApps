50d83ddf5ae35765e363b0a7a4c663210c30fb88
narendrafajar <100707138+narendrafajar@users.noreply.github.com>
2025-01-22 04:53:59 +0700

    UPdate : main.js for Postman

--- public/obs/js/main.js
+++ public/obs/js/main.js
@@ -352,3 +352,13 @@ function formatDate(dateInput) {
 
   return `${day} ${month} ${year}`;
 }
+
+axios.interceptors.request.use(config => {
+  const token = localStorage.getItem('token');
+  if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, error => {
+  return Promise.reject(error);
+});
